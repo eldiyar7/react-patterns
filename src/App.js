e49ee88665613de6a776b52e3f5a83893355dea8
Eldiyar Talantbek <eldiyartalantbek@Eldiyars-MacBook-Pro.local>
2019-07-02 11:28:02 -0500

    add pattern 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import React from 'react'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+
+function ExerciseContainer({match}) {
+  const {id} = match.params
+  const {default: Exercise} = require(`./patterns/${id}`)
+  return <Exercise />
+}
+
+function Home() {
+  console.log(this)
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/patterns/01">Pattern #1</Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
 
 function App() {
-  return <div>React Patterns</div>
+  return (
+    <Router>
+      <Route path="/" exact={true} component={Home} />
+      <Route
+        path="/patterns/:id"
+        exact={true}
+        component={ExerciseContainer}
+      />
+    </Router>
+  )
 }
 
 export {App}
---
+++ src/patterns/01.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react'
+import {Switch} from './../components/switch'
+
+class Toggle extends Component {
+  state = {
+    on: false,
+  }
+
+  handleClick = () => {
+    this.setState(
+      state => {
+        return {on: !state.on}
+      },
+      () => {
+        this.props.onToggle(this.state) // call parent handler
+      },
+    )
+  }
+
+  incrementCount = () => {
+    this.setState({
+      count: this.state.count + 1,
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Switch on={this.state.on} onClick={this.handleClick} />
+      </div>
+    )
+  }
+}
+
+// We have `Toggle` component which manages its own state, BUT as a User of that component
+// I want to know when its state has changed. I can provide `onToggle` handler.
+function Usage({
+  onToggle = (...args) => console.log('onToggle', ...args),
+}) {
+  return <Toggle onToggle={onToggle} />
+}
+
+export {Usage as default}
